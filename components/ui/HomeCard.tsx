35f575ecc34ac04e6c56fa64c936af7fb3456fc1
Ashish2343 <a98126959@gmail.com>
2025-07-07 15:26:01 +0530

    eslint-off

--- components/ui/HomeCard.tsx
+++ components/ui/HomeCard.tsx
@@ -56,7 +56,7 @@ const HomeCard = ({ className, img, title, description, handleClick }: HomeCardP
 </div>
       
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl  font-bold">{title}</h1>
+        <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-lg font-normal">{description}</p>
       </div>
     </section>
